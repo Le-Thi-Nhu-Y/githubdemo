4bed817e059f255995d18e486e5019592a2968b7
Le-Thi-Nhu-Y <lethinhuyrum@gmail.com>
2024-02-02 21:49:34 +0700

    push javascript1

---
+++ javascript/bai1/bai1.js
@@ -0,0 +1,15 @@
+
+var so_hang1 = 12
+var so_hang2 = 12
+var tong = 0
+
+tong = so_hang1 + so_hang2
+
+tong = so_hang1 + tong
+
+console.log("tong=" + tong)
+
+var width = 3
+var height = 4
+tong = width * height
+console.log("dientich=" + tong)
