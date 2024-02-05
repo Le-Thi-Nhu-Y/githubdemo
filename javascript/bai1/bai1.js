ca788b219c4fc32ea537693cd4e4fd6fed65c35e
Le-Thi-Nhu-Y <lethinhuyrum@gmail.com>
2024-02-05 21:55:30 +0700

    push javascript

--- javascript/bai1/bai1.js
+++ javascript/bai1/bai1.js
@@ -13,3 +13,36 @@ var width = 3
 var height = 4
 tong = width * height
 console.log("dientich=" + tong)
+
+
+
+function processConfirm(cau_hoi) {
+    let answer = "";
+    if (cau_hoi === "ý hỏi") {
+        answer = "Excellent. We'll play a nice game of chess.";
+    } else {
+        answer = "Maybe later then.";
+    }
+    return answer;
+}
+// console.log(processConfirm("a hoi"))
+
+
+// let confirmAnswer = confirm("Shall we play a game?");
+// let theAnswer = processConfirm(confirmAnswer);
+// // alert(theAnswer);
+// console.log(confirmAnswer)
+
+// document.write("Xin chào");
+var y = 5;
+y %= 2;
+ 
+console.log(y);
+var i = 10;
+var f = 20.5;
+var b = true
+var s = 'Hà Nội'
+document.write(i);
+document.write(f);
+document.write(b);
+document.write(s);
