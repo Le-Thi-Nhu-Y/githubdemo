74f20a1a9d7e4d1f5c4d69c192e3c8d305ca5b09
Le-Thi-Nhu-Y <lethinhuyrum@gmail.com>
2024-02-12 23:25:28 +0700

    push baif function

--- javascript/bai1/bai1.js
+++ javascript/bai1/bai1.js
@@ -1,30 +1,30 @@
 
-var so_hang1 = 12
-var so_hang2 = 12
-var tong = 0
+// var so_hang1 = 12
+// var so_hang2 = 12
+// var tong = 0
 
-tong = so_hang1 + so_hang2
+// tong = so_hang1 + so_hang2
 
-tong = so_hang1 + tong
+// tong = so_hang1 + tong
 
-console.log("tong=" + tong)
+// console.log("tong=" + tong)
 
-var width = 3
-var height = 4
-tong = width * height
-console.log("dientich=" + tong)
+// var width = 3
+// var height = 4
+// tong = width * height
+// console.log("dientich=" + tong)
 
 
 
-function processConfirm(cau_hoi) {
-    let answer = "";
-    if (cau_hoi === "ý hỏi") {
-        answer = "Excellent. We'll play a nice game of chess.";
-    } else {
-        answer = "Maybe later then.";
-    }
-    return answer;
-}
+// function processConfirm(cau_hoi) {
+//     let answer = "";
+//     if (cau_hoi === "ý hỏi") {
+//         answer = "Excellent. We'll play a nice game of chess.";
+//     } else {
+//         answer = "Maybe later then.";
+//     }
+//     return answer;
+// }
 // console.log(processConfirm("a hoi"))
 
 
@@ -34,15 +34,30 @@ function processConfirm(cau_hoi) {
 // console.log(confirmAnswer)
 
 // document.write("Xin chào");
-var y = 5;
-y %= 2;
+// var y = 5;
+// y %= 2;
  
-console.log(y);
-var i = 10;
-var f = 20.5;
-var b = true
-var s = 'Hà Nội'
-document.write(i);
-document.write(f);
-document.write(b);
-document.write(s);
+// console.log(y);
+// var i = 10;
+// var f = 20.5;
+// var b = true
+// var s = 'Hà Nội'
+// document.write(i);
+// document.write(f);
+// document.write(b);
+// document.write(s);
+
+function processConfirm(answer) {
+    let result = "";
+    if (answer) {
+        result = "Excellent. We'll play a nice game of chess.";
+    } else {
+        result = "Maybe later then.";
+    }
+    return result;
+}
+
+let confirmAnswer = confirm("Shall we play a game?");
+let theAnswer = processConfirm();
+alert(theAnswer);
+
