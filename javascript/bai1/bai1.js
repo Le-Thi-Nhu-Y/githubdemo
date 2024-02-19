63288b16d28bc8dd6679f5e7b3d1960d23f40257
Le-Thi-Nhu-Y <lethinhuyrum@gmail.com>
2024-02-19 14:19:05 +0700

    pust bai tap if else bodymass

--- javascript/bai1/bai1.js
+++ javascript/bai1/bai1.js
@@ -47,17 +47,62 @@
 // document.write(b);
 // document.write(s);
 
-function processConfirm(answer) {
-    let result = "";
-    if (answer) {
-        result = "Excellent. We'll play a nice game of chess.";
-    } else {
-        result = "Maybe later then.";
+// function processConfirm(answer) {
+//     let result = "";
+//     if (answer) {
+//         result = "Excellent. We'll play a nice game of chess.";
+//     } else {
+//         result = "Maybe later then.";
+//     }
+//     return result;
+// }
+
+// let confirmAnswer = confirm("Shall we play a game?");
+// let theAnswer = processConfirm(git);
+// alert(theAnswer);
+
+//function constructor
+
+// function User(firstName, lastName, avatar) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.avatar = avatar;
+
+//     this.getName = function() {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// var author = new User('Như', 'Ý', 'avatar');
+// var user = new User('Ý', 'Nhi', 'avatar');
+
+// User.prototype.className = 'f8' // them thuoc tinh vao ben trong ham tao
+
+// author.title = 'Như Ý xinh đẹp'
+// user.comment = 'xinh đẹp thật không?' //them thuoc tinh vao ben ngoai ham tao
+
+// console.log(author);
+// console.log(user);
+
+// baitap if else
+
+var weight = 38;
+var height = 1.52;
+var bmi = weight / (height^2)
+
+function bodyMass(bmi) {
+    if(bmi < 18.5) {
+        console.log('Underweight');
     }
-    return result;
+    else if(18.5 <= bmi < 25.0) {
+        console.log('Normal');
+    }
+    else if(25.0 <= bmi < 30.0) {
+        console.log('Overweight');
+    }
+    else {
+        console.log('Obese');
+    }
 }
 
-let confirmAnswer = confirm("Shall we play a game?");
-let theAnswer = processConfirm();
-alert(theAnswer);
-
+bodyMass(bmi);
